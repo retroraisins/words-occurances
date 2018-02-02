99508834fa02fc45723bd69f99c80a14b150fa49
retroraisins <35795338+retroraisins@users.noreply.github.com>
2018-02-02 15:12:28 +0300

    Update word_occuarance.py
    add input file check

--- word_occuarance.py
+++ word_occuarance.py
@@ -8,17 +8,24 @@ VALUE = 1
 
 def get_words_from(path):
     words = []
-    with open(path) as file:
-        for line in file:
-            words += re.sub("[^\w]", " ", line).split()
+    try:
+        with open(path) as file:
+            for line in file:
+                words += re.sub("[^\w]", " ", line).split()
+            return words
+    except IOError:
+        sys.exit("No such file")
+
+
+def get_words_from_input():
+    words = []
+    for line in sys.stdin:
+        words += re.sub("[^\w]", " ", line).split()
     return words
 
 
-if __name__ == "__main__":
-    file_path = sys.argv[1]
-    words = get_words_from(file_path)
+def get_words_frecuency(words):
     words_frequency = {}
-
     for word in words:
         if str(word) in words_frequency:
             words_frequency[str(word)] += 1
@@ -30,3 +37,15 @@ if __name__ == "__main__":
     for item in sorted(
             words_frequency_sorted, key=itemgetter(1), reverse=True):
         print("{}: {}".format(item[KEY], item[VALUE]))
+
+
+if __name__ == "__main__":
+    try:
+        file_path = sys.argv[1]
+        words = get_words_from(file_path)
+    except IndexError:
+        words = get_words_from_input()
+    if words:
+        get_words_frecuency(words)
+    else:
+        print("Empty input")
