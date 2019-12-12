a12e3a747a7de875b08aed343b4f9cd8316a2f1e
RetroRaisins <retroraisins@gmail.com>
2019-12-12 16:02:15 +0300

    [upd] refactoring

--- words_frequency.py
+++ words_frequency.py
@@ -3,26 +3,12 @@ import re
 from operator import itemgetter
 from collections import Counter
 
-KEY = 0
-VALUE = 1
-
-
-def get_words_from(path):
-    words = []
-    with open(path) as file:
-        for line in file:
-            words += re.sub("[^\w]", " ", line).split()
-    return words
-
-
 if __name__ == "__main__":
     file_path = sys.argv[1]
-    words = get_words_from(file_path)
+    words = re.findall(r'\w+', open(file_path, 'r').read().lower())
 
-    words_frequency = Counter(words)
+    words_frequency_sorted = sorted(Counter(words).items(), key=itemgetter(0))
 
-    words_frequency_sorted = sorted(words_frequency.items(), key=itemgetter(0))
-
-    for item in sorted(
+    for word, frequency in sorted(
             words_frequency_sorted, key=itemgetter(1), reverse=True):
-        print("{}: {}".format(item[KEY], item[VALUE]))
+        print("{}: {}".format(word, frequency))
